7bf1a15ea7b587e3a3c365d003cfae8491c4f2c9
NoRestFortheWickedBuilds <danboobar@gmail.com>
2025-05-29 02:28:29 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>No Rest for the Wicked Builds</h1>
+      <p>This is the beginning of something great.</p>
+    </div>
+  );
+}
