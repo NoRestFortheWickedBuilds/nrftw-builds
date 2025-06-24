206b6929d4452507348197fde4bccc941233ac6f
Boo Bear <placeholder@example.com>
2025-06-24 15:37:50 -0500

    Fix landing page to use new image icons

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,20 @@
 
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import React from 'react';
+import '../styles/globals.css';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>WickedGuides - NRFTW Builds</title>
-      </Head>
+const Home = () => (
+  <div className="landing-page">
+    <h1>WickedGuides</h1>
+    <nav>
+      <a href="/builds/coldmage"><img src="/icons/coldmage-icon.png" alt="Cold Mage" className="icon" /> Cold Mage</a>
+      <a href="/builds/firemage"><img src="/icons/firemage-icon.png" alt="Fire Mage" className="icon" /> Fire Mage</a>
+      <a href="/builds/lightningmage"><img src="/icons/lightningmage-icon.png" alt="Lightning Mage" className="icon" /> Lightning Mage</a>
+      <a href="/builds/juggernaut"><img src="/icons/juggernaut-icon.png" alt="Juggernaut" className="icon" /> Juggernaut</a>
+      <a href="/builds/meleetank"><img src="/icons/meleetank-icon.png" alt="Melee Tank" className="icon" /> Melee-Tank Build</a>
+      <a href="/builds/bow"><img src="/icons/bow-icon.png" alt="Bow Build" className="icon" /> Bow Build</a>
+      <a href="/builds/ninjawakizashi"><img src="/icons/ninjawakizashi-icon.png" alt="Ninja Wakizashi" className="icon" /> Ninja-Wakizashi Build</a>
+    </nav>
+  </div>
+);
 
-      <header className={styles.header}>
-        <img src="/header-background.jpeg" alt="Header Background" className={styles.headerBackground} />
-        <h1 className={styles.title}>WickedGuides</h1>
-      </header>
-
-      <nav className={styles.navbar}>
-        <a href="#">Meta Builds</a>
-        <a href="#">Leveling Guide</a>
-        <a href="#">TierList</a>
-        <a href="#">My Builds</a>
-        <a href="#">Build Planner</a>
-        <a href="#">Map</a>
-        <a href="#">Database</a>
-      </nav>
-
-      <main className={styles.main}>
-        <h2 className={styles.sectionTitle}>Meta Builds</h2>
-        <div className={styles.buildList}>
-          <a href="#" className={styles.buildCard}>❄ Cold Mage</a>
-          <a href="#" className={styles.buildCard}>🔥 Fire Mage</a>
-          <a href="#" className={styles.buildCard}>⚡ Lightning Mage</a>
-          <a href="#" className={styles.buildCard}>🛡 Juggernaut</a>
-          <a href="#" className={styles.buildCard}>⚔ Melee-Tank Build</a>
-          <a href="#" className={styles.buildCard}>🏹 Bow Build</a>
-          <a href="#" className={styles.buildCard}>🗡 Ninja-Wakizashi Build</a>
-        </div>
-      </main>
-    </div>
-  );
-}
+export default Home;
