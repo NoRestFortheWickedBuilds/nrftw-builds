fdfc2d03c2c0a5a5e833a80d714157fd35178463
Boo Bear <placeholder@example.com>
2025-06-24 14:36:16 -0500

    Add Ninja-Wakizashi and Melee-Tank build pages

---
+++ pages/builds/meleetank.js
@@ -0,0 +1,105 @@
+
+import React from 'react';
+
+const MeleeTank = () => (
+  <div className="build-page">
+    <h1>MeleeTank Build</h1>
+    <p><strong>By:</strong> Larry Puss</p>
+    <p><a href="https://youtu.be/KuJYeqXWypo?si=vvehmFUOxAMLXDd-" target="_blank">Watch Build Guide on YouTube</a></p>
+    <div class='character-stats-box'>
+<h2>Character Stats</h2>
+<ul>
+  <li><strong>Health:</strong> 40</li>
+  <li><strong>Stamina:</strong> 10</li>
+  <li><strong>Strength:</strong> 50</li>
+  <li><strong>Dexterity:</strong> 10</li>
+  <li><strong>Intelligence:</strong> 10</li>
+  <li><strong>Faith:</strong> 10</li>
+  <li><strong>Focus:</strong> 17</li>
+  <li><strong>Equip Load:</strong> 20</li>
+</ul>
+</div>
+
+    <div class='gear-section'>
+<h2>Gear & Enchantments</h2>
+<h3>Weapon 1: Tongue Splitter</h3>
+<ul>
+  <li><strong>Runes:</strong> Stormpiercer, Damage Surge, Tornado Strike</li>
+  <li><strong>Gem:</strong> Lifesteal increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Increased 30% against Large Enemies</li>
+      <li>Healing increased by up to 20% based on Durability</li>
+      <li>Stamina Recovery increased by up to 25% based on Durability</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Off-Hand: Fallstead's Barricade</h3>
+<ul>
+  <li><strong>Gem:</strong> Physical Damage increased by 5%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Deal 25% Damage on Block</li>
+      <li>Gain 10% Focus on Block</li>
+      <li>Indestructible</li>
+    </ul>
+  </li>
+</ul>
+<h3>Helmet: Plate (Blue)</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Stamina increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Focus Gain Increased by 15%</li>
+      <li>Poise Defense increased by 3</li>
+      <li>Stagger Resistance increased by 15%</li>
+    </ul>
+  </li>
+</ul>
+<h3>Body Armor: Plate (Blue)</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Stamina increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Focus Gain Increased by 15%</li>
+      <li>Poise Defense increased by 3</li>
+      <li>Stagger Resistance increased by 15%</li>
+    </ul>
+  </li>
+</ul>
+<h3>Gloves: Plate (Blue)</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Health increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Armor increased by 20%</li>
+      <li>Poise Defense increased by 3</li>
+      <li>Stagger Resistance increased by 15%</li>
+    </ul>
+  </li>
+</ul>
+<h3>Pants: Plate (Blue)</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Stamina increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Physical Damage increased by 10%</li>
+      <li>Poise Defense increased by 3</li>
+      <li>Stagger Resistance increased by 15%</li>
+    </ul>
+  </li>
+</ul>
+<h3>Ring 1: Plagued Ring</h3>
+<ul>
+</ul>
+<h3>Ring 2: Snake Ring</h3>
+<ul>
+</ul>
+</div>
+
+  </div>
+);
+
+export default MeleeTank;
---
+++ pages/builds/ninjawakizashi.js
@@ -0,0 +1,114 @@
+
+import React from 'react';
+
+const NinjaWakizashi = () => (
+  <div className="build-page">
+    <h1>NinjaWakizashi Build</h1>
+    <p><strong>By:</strong> Larry Puss</p>
+    <p><a href="https://youtu.be/9yTT5rR4fLY?si=1G8ItK40kzXskBu7" target="_blank">Watch Build Guide on YouTube</a></p>
+    <div class='character-stats-box'>
+<h2>Character Stats</h2>
+<ul>
+  <li><strong>Health:</strong> 10</li>
+  <li><strong>Stamina:</strong> 10</li>
+  <li><strong>Strength:</strong> 10</li>
+  <li><strong>Dexterity:</strong> 50</li>
+  <li><strong>Intelligence:</strong> 10</li>
+  <li><strong>Faith:</strong> 50</li>
+  <li><strong>Focus:</strong> 13</li>
+  <li><strong>Equip Load:</strong> 14</li>
+</ul>
+</div>
+
+    <div class='gear-section'>
+<h2>Gear & Enchantments</h2>
+<h3>Weapon 1: Wind of Death</h3>
+<ul>
+  <li><strong>Runes:</strong> Plague Whirl, Jump Cut, Slashing Dash, Twirl Dash</li>
+  <li><strong>Gem:</strong> Electric Damage Infusion</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage increased by up to 60% based on Missing Health</li>
+      <li>Dodge Stamina Cost decreased by 30%</li>
+      <li>Lose 5% Health on Damage Dealt</li>
+    </ul>
+  </li>
+</ul>
+<h3>Off-Hand: Short Bow</h3>
+<ul>
+  <li><strong>Runes:</strong> Arrow, Muti Shot, Electric Enchantment, DAmage Surge</li>
+  <li><strong>Gem:</strong> Lifesteal</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage increased by 30% for 6 seconds after Damage Dodged</li>
+      <li>Stamina Recovery increased by up to 25% based on Durability</li>
+      <li>Gain 5% Health on Damage Dodged</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Helmet: Leather</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Stamina increased by 25%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Electric Damage increased by 25%</li>
+      <li>Gain 6% Health once per Rune Attack</li>
+      <li>Gain 15 Focus on inflicting Any Buildup</li>
+      <li>Stamina Recovery increased by up to 25% based on Durability</li>
+      <li>Drain Health in Combat</li>
+    </ul>
+  </li>
+</ul>
+<h3>Body Armor: Leather</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Health increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Deal 15% Damage on Damage Taken</li>
+      <li>Focus Gain  increased by up to 20% basded on Durability</li>
+      <li>Stamina Recovery increased by up to 25% based on Durability</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Gloves: Leather</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Stamina increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Focus Gain  increased by up to 20% basded on Durability</li>
+      <li>Gain 10% Health on Kill</li>
+      <li>Physical Damage increased by 25%</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Pants: Leather</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Stamina increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage increased by 30% for 6 seconds after Damage Dodged</li>
+      <li>Focus Gain  increased by up to 20% basded on Durability</li>
+    </ul>
+  </li>
+</ul>
+<h3>Ring 1: Agility Ring</h3>
+<ul>
+</ul>
+<h3>Ring 2: Plagued Ring</h3>
+<ul>
+</ul>
+<h3>Ring 3: Willow Cap Ring</h3>
+<ul>
+</ul>
+</div>
+
+  </div>
+);
+
+export default NinjaWakizashi;
