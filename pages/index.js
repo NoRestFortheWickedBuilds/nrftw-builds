b420d9817cfabc93d764ea726b3b7a85556058cc
Boo Bear <placeholder@example.com>
2025-06-24 16:25:45 -0500

    Activate build page links on landing page

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = () => (
       <a href="/builds/firemage"><img src="/icons/firemage-icon.png" alt="Fire Mage" className="icon" /> Fire Mage</a>
       <a href="/builds/lightningmage"><img src="/icons/lightningmage-icon.png" alt="Lightning Mage" className="icon" /> Lightning Mage</a>
       <a href="/builds/juggernaut"><img src="/icons/juggernaut-icon.png" alt="Juggernaut" className="icon" /> Juggernaut</a>
-      <a href="/builds/meleetank"><img src="/icons/meleetank-icon.png" alt="Melee Tank" className="icon" /> Melee-Tank Build</a>
+      <a href="/builds/meleetank"><img src="/icons/meleetank-icon.png" alt="Melee-Tank Build" className="icon" /> Melee-Tank Build</a>
       <a href="/builds/bow"><img src="/icons/bow-icon.png" alt="Bow Build" className="icon" /> Bow Build</a>
       <a href="/builds/ninjawakizashi"><img src="/icons/ninjawakizashi-icon.png" alt="Ninja Wakizashi" className="icon" /> Ninja-Wakizashi Build</a>
     </nav>
