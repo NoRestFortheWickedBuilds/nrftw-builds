367e3d6c5d0756d0752cacb2614b8b171eb774ed
Boo Bear <placeholder@example.com>
2025-06-24 14:26:48 -0500

    Add Fire Mage build page

---
+++ pages/builds/firemage.js
@@ -0,0 +1,131 @@
+
+import React from 'react';
+
+const FireMage = () => (
+  <div className="build-page">
+    <h1>Fire Mage Build</h1>
+    <p><strong>By:</strong> EasyMode Gaming</p>
+    <p><a href="https://youtu.be/Ea_Nz5cca4s?si=PMTV95euzU9yT5Iv" target="_blank">Watch Build Guide on YouTube</a></p>
+    <div class='character-stats-box'>
+<h2>Character Stats</h2>
+<ul>
+  <li><strong>Health:</strong> 30</li>
+  <li><strong>Stamina:</strong> 10</li>
+  <li><strong>Strength:</strong> 10</li>
+  <li><strong>Dexterity:</strong> 10</li>
+  <li><strong>Intelligence:</strong> 71</li>
+  <li><strong>Faith:</strong> 10</li>
+  <li><strong>Focus:</strong> 10</li>
+  <li><strong>Equip Load:</strong> 16</li>
+</ul>
+</div>
+
+    <div class='gear-section'>
+<h2>Gear & Enchantments</h2>
+<h3>Weapon 1: Molten Clutch</h3>
+<ul>
+  <li><strong>Runes:</strong> Fireblast, Pulse of Health, Damage Surge, Blink</li>
+  <li><strong>Gem:</strong> Lifesteal</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Healing Increased by 20% based on Durability</li>
+      <li>Rune Damage increased by 20%</li>
+      <li>Item Weight decreased by 15%</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Weapon 2: Rune Stricken Wand</h3>
+<ul>
+  <li><strong>Runes:</strong> Illuminate, Repair, Return, Poise Shield</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Attack Stamina Cost decreased by % for 6 seconds after Parry</li>
+      <li>Damage increased by up to 15% based on Durability</li>
+      <li>Gain 6 Focus on Backstab</li>
+      <li>Movement Speed increased by 15% for 6 seconds after Rune Attack</li>
+      <li>Attack Stamina Cost increased by 26%</li>
+    </ul>
+  </li>
+</ul>
+<h3>Off-Hand: Shield</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Health increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage increased by up to 15% based on Durability</li>
+      <li>Damage Taken decreased by 10% while blocking</li>
+      <li>Stamina Recovery increased by upt to 15% based on Durability</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Helmet: Plate</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Health increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by 10% based on Durability</li>
+      <li>Gain 15 Focus on inflicting Any Buildup</li>
+      <li>Healing increased by up to 20% base on Durability</li>
+      <li>Heat Damage increased by 25%</li>
+      <li>Stamina Cost increased by 19%</li>
+    </ul>
+  </li>
+</ul>
+<h3>Body Armor: Plate</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Health increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by 10% based on Durability</li>
+      <li>Focus Gain increased by up to 20% based on Durability</li>
+      <li>Burn Damage increased by 50%</li>
+      <li>Item Weight decreased by 15%</li>
+      <li>Drain Focus in Combat</li>
+    </ul>
+  </li>
+</ul>
+<h3>Gloves: Mesh</h3>
+<ul>
+  <li><strong>Gem:</strong> Focus Cost decreased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by 10% based on Durability</li>
+      <li>Focus Gain increased by up to 20% based on Durability</li>
+      <li>Gain 10% Health on Kill</li>
+      <li>Item Weight decreased by 15%</li>
+      <li>Parry Disabled</li>
+    </ul>
+  </li>
+</ul>
+<h3>Pants: Plate</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Health increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by 10% based on Durability</li>
+      <li>Damage increased by 30% for 6 seconds after Damage Dodged</li>
+      <li>Item Weight decreased by 15%</li>
+      <li>Healing increased by up to 20% base on Durability</li>
+      <li>Drain Health in Combat</li>
+    </ul>
+  </li>
+</ul>
+<h3>Ring 1: Ring of Broken Promises</h3>
+<ul>
+</ul>
+<h3>Ring 2: Rune Ring</h3>
+<ul>
+</ul>
+<h3>Ring 3: Ring of Determination</h3>
+<ul>
+</ul>
+</div>
+
+  </div>
+);
+
+export default FireMage;
