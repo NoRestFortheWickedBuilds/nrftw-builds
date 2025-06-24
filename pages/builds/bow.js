3568c87f21b2c5a55ce5e37203f8f9aca92202ce
Boo Bear <placeholder@example.com>
2025-06-24 14:34:30 -0500

    Add Bow, Juggernaut, and Lightning Mage build pages

---
+++ pages/builds/bow.js
@@ -0,0 +1,118 @@
+
+import React from 'react';
+
+const Bow = () => (
+  <div className="build-page">
+    <h1>Bow Build</h1>
+    <p><strong>By:</strong> Zelendes</p>
+    <p><a href="https://youtu.be/cMatlgi0ziQ?si=pz4YgbDjKFfOfA1m" target="_blank">Watch Build Guide on YouTube</a></p>
+    <div class='character-stats-box'>
+<h2>Character Stats</h2>
+<ul>
+  <li><strong>Health:</strong> 10</li>
+  <li><strong>Stamina:</strong> 10</li>
+  <li><strong>Strength:</strong> 10</li>
+  <li><strong>Dexterity:</strong> 50</li>
+  <li><strong>Intelligence:</strong> 10</li>
+  <li><strong>Faith:</strong> 50</li>
+  <li><strong>Focus:</strong> 13</li>
+  <li><strong>Equip Load:</strong> 14</li>
+</ul>
+</div>
+
+    <div class='gear-section'>
+<h2>Gear & Enchantments</h2>
+<h3>Weapon 1: Falstead's Fury</h3>
+<ul>
+  <li><strong>Runes:</strong> Triple Swipe, Kick Slash, Crushing Slam, Damage Surge</li>
+  <li><strong>Gem:</strong> Item Weight decreased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage increased by 20% against Large Enemies</li>
+      <li>Damage increased by 30% at Full Health</li>
+      <li>Max Health increased by 20%</li>
+      <li>Deal 24% Damage on Parry</li>
+    </ul>
+  </li>
+</ul>
+<h3>Off-Hand: Woodland Protector</h3>
+<ul>
+  <li><strong>Runes:</strong> Arrow, Lightning Arrow, Cone Shot</li>
+  <li><strong>Gem:</strong> Electric Damage Infusion</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Dmage increased by up to 15% based on Durability</li>
+      <li>Gain 5% Health on Damage Dodged</li>
+      <li>Item Weight decreased by 15%</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Helmet: Leather</h3>
+<ul>
+  <li><strong>Gem:</strong> Shock Buildup increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by up to 10% based on Durability</li>
+      <li>Electric Damage increased by 25%</li>
+      <li>Gain 15 Focus on inflicting Any Buildup</li>
+      <li>Healing increased by up to 20% based on Durability</li>
+      <li>Any</li>
+    </ul>
+  </li>
+</ul>
+<h3>Body Armor: Cloth</h3>
+<ul>
+  <li><strong>Gem:</strong> Focus Cost decreased by 5%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by 50% for 6 seconds after being afflicted by Any Buildup</li>
+      <li>Damage increased by 30% for 6 seconds after Staggering an Enemy</li>
+      <li>Overall Speed increased by 15% for 10 seconds after inflicting Shock Buildup</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Gloves: Mesh</h3>
+<ul>
+  <li><strong>Gem:</strong> Shock Buildup increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by up to 10% based on Durability</li>
+      <li>Gain 10% Health on Kill</li>
+      <li>Shock Buildup increased by 30%</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Pants: Mesh</h3>
+<ul>
+  <li><strong>Gem:</strong> Focus Cost decreased by 5%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage increased by 10% after Sprinting for 2 seconds</li>
+      <li>Damage Taken decreased by up to 10% based on Durability</li>
+      <li>Item Weight decreased by 15%</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Ring 1: Rune Ring</h3>
+<ul>
+</ul>
+<h3>Ring 2: Rune of Broken Promises</h3>
+<ul>
+</ul>
+<h3>Ring 3: Willow Cap Ring</h3>
+<ul>
+</ul>
+</div>
+
+  </div>
+);
+
+export default Bow;
---
+++ pages/builds/juggernaut.js
@@ -0,0 +1,117 @@
+
+import React from 'react';
+
+const Juggernaut = () => (
+  <div className="build-page">
+    <h1>Juggernaut Build</h1>
+    <p><strong>By:</strong> Larry Puss</p>
+    <p><a href="https://youtu.be/tehiq6Jflik?si=cJEiVdK3tGBBsP3M" target="_blank">Watch Build Guide on YouTube</a></p>
+    <div class='character-stats-box'>
+<h2>Character Stats</h2>
+<ul>
+  <li><strong>Health:</strong> 40</li>
+  <li><strong>Stamina:</strong> 10</li>
+  <li><strong>Strength:</strong> 10</li>
+  <li><strong>Dexterity:</strong> 10</li>
+  <li><strong>Intelligence:</strong> 10</li>
+  <li><strong>Faith:</strong> 57</li>
+  <li><strong>Focus:</strong> 10</li>
+  <li><strong>Equip Load:</strong> 20</li>
+</ul>
+</div>
+
+    <div class='gear-section'>
+<h2>Gear & Enchantments</h2>
+<h3>Weapon 1: Cursed Unguis</h3>
+<ul>
+  <li><strong>Runes:</strong> Plague Retch, Frontflip Kick, Damage Surge, Dropkick</li>
+  <li><strong>Gem:</strong> Cold Damage Infusion</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Attack Speed Increased by up to 15% based on Durability</li>
+      <li>Healing increased by up to 20% based on Durability</li>
+      <li>Stamina Recovery increased by up to 25% based on Durability</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Weapon 2: Fetid Club</h3>
+<ul>
+  <li><strong>Runes:</strong> Crushing Quad, Pole Flurry, Whirlwind, Berserk Strike</li>
+  <li><strong>Gem:</strong> Physical Damage increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage increased by 10% of Current Weight</li>
+      <li>Damage Taken decreased by 20% while Charging</li>
+      <li>Refill Stamina on Staggering an Enemy</li>
+    </ul>
+  </li>
+</ul>
+<h3>Helmet: Plate</h3>
+<ul>
+  <li><strong>Gem:</strong> Frost Buildup increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Cold Damage increased by 25%</li>
+      <li>Damage Taken decreased by up to 10% based on Durability</li>
+      <li>Stamina Recovery increased by up to 25% based on Durability</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Body Armor: Plate</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Stamina increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Frozen Enemy Armor decreased by 50%</li>
+      <li>Max Health increased by 10% of Max Focus</li>
+      <li>Stamina Recovery increased by up to 25% based on Durability</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Gloves: Plate</h3>
+<ul>
+  <li><strong>Gem:</strong> Frost Buildup increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Frost Buildup increased by 30%</li>
+      <li>Damage Taken decreased by up to 10% based on Durability</li>
+      <li>Stamina Recovery increased by up to 25% based on Durability</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Pants: Plate</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Stamina increased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage increased by 30% for 6 seconds after Damage Dodged</li>
+      <li>Damage Taken decreased by up to 10% based on Durability</li>
+      <li>Max Stamina increased by 5% of Max Focus</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Ring 1: Plagued Ring</h3>
+<ul>
+</ul>
+<h3>Ring 2: Willowcap Ring</h3>
+<ul>
+</ul>
+<h3>Ring 3: Jade Ring</h3>
+<ul>
+</ul>
+</div>
+
+  </div>
+);
+
+export default Juggernaut;
