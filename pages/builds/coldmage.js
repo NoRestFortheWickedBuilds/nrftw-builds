efc52ea04b9556dcbbfebf2fbb00abbb21fbc69b
Boo Bear <placeholder@example.com>
2025-06-24 11:01:12 -0500

    Clean rebuild: added Cold Mage build

---
+++ pages/builds/coldmage.js
@@ -0,0 +1,108 @@
+
+import React from 'react';
+
+const ColdMage = () => (
+  <div className="build-page">
+    <div class='character-stats-box'>
+<h2>Character Stats</h2>
+<ul>
+  <li><strong>Health:</strong> 40</li>
+  <li><strong>Stamina:</strong> 10</li>
+  <li><strong>Strength:</strong> 10</li>
+  <li><strong>Dexterity:</strong> 10</li>
+  <li><strong>Intelligence:</strong> 10</li>
+  <li><strong>Faith:</strong> 55</li>
+  <li><strong>Focus:</strong> 20</li>
+  <li><strong>Equip Load:</strong> 12</li>
+</ul>
+</div>
+
+    <div class='gear-section'>
+<h2>Gear & Enchantments</h2>
+<h3>Weapon 1: Endless Winter</h3>
+<ul>
+  <li><strong>Runes:</strong> Homing Frost, Frost Stream, Homing Flurry</li>
+  <li><strong>Gem:</strong> Lifesteal</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage increased by 30% against Large Enemies</li>
+      <li>Gain 15 Focus on Kill</li>
+      <li>Stamina Recovery increased by up to 25% based on Durability</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Weapon 2: nan</h3>
+<ul>
+</ul>
+<h3>Off-Hand: nan</h3>
+<ul>
+</ul>
+<h3>Helmet: Cloth</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Focus increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Cold Damage increased by 25%</li>
+      <li>Gain 15 Focus on inflicting any buildup</li>
+      <li>Gain 6% Health once per Rune Attack</li>
+      <li>Item Weight decreased by 15%</li>
+      <li>Any</li>
+    </ul>
+  </li>
+</ul>
+<h3>Body Armor: Cloth</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Health increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by 10% based on Durability</li>
+      <li>Focus Gain increased by up to 20% based on Durability</li>
+      <li>Frozen Enemy Armor decreased by 50%</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Gloves: Cloth</h3>
+<ul>
+  <li><strong>Gem:</strong> Focus Cost decreased by 10%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by 10% based on Durability</li>
+      <li>Focus Gain increased by up to 20% based on Durability</li>
+      <li>Frost Buildup increased by 30%</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Pants: Cloth</h3>
+<ul>
+  <li><strong>Gem:</strong> Max Stamina increased by 15%</li>
+  <li><strong>Enchantments:</strong>
+    <ul>
+      <li>Damage Taken decreased by 10% based on Durability</li>
+      <li>Focus Gain increased by up to 20% based on Durability</li>
+      <li>Item Weight decreased by 15%</li>
+      <li>Indestructible</li>
+      <li>Unrepairable</li>
+    </ul>
+  </li>
+</ul>
+<h3>Ring 1: Ring of Broken Promises</h3>
+<ul>
+</ul>
+<h3>Ring 2: Rune Ring</h3>
+<ul>
+</ul>
+<h3>Ring 3: Plagued Ring</h3>
+<ul>
+</ul>
+</div>
+
+  </div>
+);
+
+export default ColdMage;
