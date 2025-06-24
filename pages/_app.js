a7ab8fbce989e154c7fa47405025954016720b7a
Boo Bear <placeholder@example.com>
2025-06-24 11:37:43 -0500

    Add _app.js for global CSS import

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+
+import '../styles/build-style.css';
+import '../styles/globals.css'; // If you have globals.css, otherwise remove this line
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
