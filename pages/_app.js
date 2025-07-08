383a532aec65f9ed7c3a91ee2a86983d20284bc9
Boo Bear <placeholder@example.com>
2025-07-08 02:36:30 -0500

    Fixed global CSS import for mouseover build

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/build-style.css';
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/melee-tank.js
@@ -0,0 +1,8 @@
+export default function MeleeTankBuild() {
+  return (
+    <div>
+      <h1>Melee Tank Build</h1>
+      {/* Gear and mouseover UI will go here */}
+    </div>
+  );
+}
